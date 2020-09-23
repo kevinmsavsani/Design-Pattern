bf8e391fe3ce5b9da8d3e94205ca1a5acae5d1b2
kevinmsavsani <kevinmsavsani@gmail.com>
2020-09-23 23:35:05 +0530

    Few Design

---
+++ strategy/Strategy.java
@@ -0,0 +1,4 @@
+package strategy;
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
---
+++ strategy/StrategyPatternDemo.java
@@ -0,0 +1,19 @@
+package strategy;
+
+import strategy.Context;
+import strategy.OperationAdd;
+import strategy.OperationMultiply;
+import strategy.OperationSubstract;
+
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubstract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+}
